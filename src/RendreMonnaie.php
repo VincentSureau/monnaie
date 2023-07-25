925a5b4e8ba309f6e3893de6868211b6ca101216
vincent sureau <vincentsureau5@gmail.Com>
2023-07-25 10:18:41 +0200

    ajouté solution simple

--- src/RendreMonnaie.php
+++ src/RendreMonnaie.php
@@ -4,4 +4,54 @@ namespace App;
 
 class RendreMonnaie
 {
+    function rendreMonnaie($montantDonne, $prixAPayer): string {
+        $billets = [5, 10, 20, 50, 100, 200];
+        $pieces = [0.01, 0.02, 0.05, 0.10, 0.20, 0.50, 1, 2 ];
+        $reste = null;
+        
+        if($montantDonne < $prixAPayer) {
+            throw new \Exception('Le montant donné est insuffisant');
+        }
+        if($montantDonne == $prixAPayer) {
+            return 'Montant à rendre : 0€';
+        }
+        if($prixAPayer == 0) {
+            return "Montant à rendre : {$montantDonne}€";
+        }
+        $reste = $montantDonne - $prixAPayer;
+        $billets = array_reverse($billets);
+        $pieces = array_reverse($pieces);
+        $message = "Montant à rendre : {$reste}€";
+        // pour chaque billet 
+            // vérifier que montant à rendre est >= aux billets
+                // calculer le nombre de billets à rendre
+                // calculer le nouveau resteARendre
+        // pour chaque piece
+            // vérifier que montant à rendre est >= aux pièces
+                // calculer le nombre de pièces à rendre
+                // calculer le nouveau reste à rendre
+
+        foreach ($billets as $key => $value) {
+            if($reste >= $value) {
+                $nbBillets = floor($reste / $value);
+                $reste = $reste - ($value * $nbBillets);
+                $message .= "\nNombre de billet de {$value}€ : {$nbBillets}"; 
+            }
+        }
+        $reste *= 100;
+        foreach ($pieces as $key => $value) {
+            $coin = $value;
+            $value *=100;
+            if($reste >= $value) {
+                $nbPieces = floor($reste / $value);
+                $reste = $reste - ($value * $nbPieces);
+                if($value < 100) {
+                    $message .= "\nNombre de pièces de {$value}cts : {$nbPieces}"; 
+                } else {
+                    $message .= "\nNombre de pièces de {$coin}€ : {$nbPieces}"; 
+                }
+            }
+        }
+        return $message;
+    }
 }
--- tests/src/RendreMonnaieTest.php
+++ tests/src/RendreMonnaieTest.php
@@ -2,6 +2,7 @@
 
 use App\RendreMonnaie;
 use PHPUnit\Framework\TestCase;
+use tests\LoggerTrait;
 
 class RendreMonnaieTest extends TestCase
 {
@@ -10,4 +11,53 @@ class RendreMonnaieTest extends TestCase
         $this->assertTrue(class_exists(RendreMonnaie::class));
     }
 
+    public function testMontantInsuffisant()
+    {
+        $this->expectException(\Exception::class);
+        $rendreMonnaie = new RendreMonnaie();
+        $rendreMonnaie->rendreMonnaie(20, 27.80);
+    }
+
+    public function testMontantExact()
+    {
+        $rendreMonnaie = new RendreMonnaie();
+        $message = $rendreMonnaie->rendreMonnaie(38.50, 38.50);
+        $this->assertEquals('Montant à rendre : 0€', $message);
+    }
+
+    public function testPrixAPayerNul()
+    {
+        $rendreMonnaie = new RendreMonnaie();
+        $message = $rendreMonnaie->rendreMonnaie(10, 0);
+        $this->assertEquals('Montant à rendre : 10€', $message);
+    }
+
+    public function testMontantDonneSuffisant()
+    {
+        $rendreMonnaie = new RendreMonnaie();
+        $expected = str_replace("\r", '', <<<END
+            Montant à rendre : 74€
+            Nombre de billet de 50€ : 1
+            Nombre de billet de 20€ : 1
+            Nombre de pièces de 2€ : 2
+            END
+        );
+        $message = $rendreMonnaie->rendreMonnaie(200, 126);
+        $this->assertEquals($expected, $message);
+    }
+
+    public function testRendueDecimal()
+    {
+        $rendreMonnaie = new RendreMonnaie();
+        $expected = str_replace("\r", '', <<<END
+            Montant à rendre : 50.25€
+            Nombre de billet de 50€ : 1
+            Nombre de pièces de 20cts : 1
+            Nombre de pièces de 5cts : 1
+            END
+        );
+        $message = $rendreMonnaie->rendreMonnaie(100, 49.75);
+        $this->assertEquals($expected, $message);
+
+    }
 }
